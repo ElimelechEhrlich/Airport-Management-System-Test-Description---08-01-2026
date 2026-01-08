54f623aa23ea8d6815504ce4922b4ea7247d4a45
Elimelech Ehrlich <ee525566@gmail.com>
2026-01-08 12:59:30 +0200

    create classes and functions

---
+++ main.test.js
@@ -0,0 +1,9 @@
+import { test } from "node:test"
+import { assert } from "node:assert"
+
+test.describe("", () => {
+    test("buyingTicket", () => {
+        
+        assert.strictEqual(buyingTicket())
+    })
+})
